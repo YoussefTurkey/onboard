a0ef42979d2c806b69e1803e5ab68029801ed443
YoussefTurkey <youssef.turkey11@gmail.com>
2024-12-18 22:37:14 +0200

    fix responsive blogs section

--- src/app/(pages)/blogs/page.jsx
+++ src/app/(pages)/blogs/page.jsx
@@ -19,7 +19,7 @@ const data = {
       post: [
         {id: 1,src: '/images/photos/post_1.webp', title: 'Graphic Design Trends', date: '13 May 2024', category:'Graphic Desing', to: '/'},
         {id: 2,src: '/images/photos/post_2.webp', title: 'Web Development', date: '10 May 2024', category:'Web Development', to: '/'},
-        {id: 3,src: '/images/photos/post_3.webp', title: 'Visual Identity Branding', date: '08 May 2024', category:'Graphic Desing', to: '/'},
+        {id: 3,src: '/images/photos/post_3.webp', title: 'Visual Identity Branding', date: '08 May 2024', category:'Graphic Desing', to: '/', style: 'expand'},
       ]
     }
 }
@@ -32,12 +32,12 @@ const Blogs = () => {
         <section className={`${styles.blogs} container mx-auto my-40 flex flex-col items-center`} id='blogs'>
             <Title data={data} />
 
-            <div className={`${styles.blogs} grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-[10px] sm:gap-[20px] md:gap-[30px] my-10 md:mx-[85px] justify-items-center`}>
+            <div className={`${styles.blogs} grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-[10px] sm:gap-[10px] md:gap-[30px] my-10 md:mx-[85px] justify-items-center`}>
             {
                 posts.map( post => {
                     return(
-                        <Link href={post.to ? post.to : ''} key={post.id} className={`${styles.card} flex flex-col items-start gap-[15px] bg-bgHover border border-bgHover hover:border-primary rounded-3xl w-[250px] md:w-[100%] transition-all`}>
-                            <Image src={post.src} alt={post.title} loading='lazy' width='300' height='300' className='w-[100%] h-[300px] rounded-tr-3xl rounded-tl-3xl border-b-[8px] border-primary object-cover' />
+                        <Link href={post.to ? post.to : ''} key={post.id} className={`${styles.blog} ${post.style ? styles.expand : ''} flex flex-col items-start bg-bgHover border border-bgHover hover:border-primary rounded-3xl w-[100%] my-3 sm:my-0 transition-all`}>
+                            <Image src={post.src} alt={post.title} loading='lazy' width='300' height='300' className='w-[100%] h-[150px] sm:h-[300px] rounded-tr-3xl rounded-tl-3xl border-b-[8px] border-primary object-cover' />
                             <div className="p-5">
                                 <h4 className='text-primary text-[24px] font-[900] py-2'>{post.title}</h4>
                                 <p className='text-secondary text-[18px] font-[400] flex items-center py-2'> <span className='text-primary text-[18px] pr-3'><FaRegCalendarDays /></span>{post.date}</p>
--- src/app/(pages)/contact/page.jsx
+++ src/app/(pages)/contact/page.jsx
@@ -24,7 +24,7 @@ const data = {
       cards: [
         {id: 1,icon: <BiPhoneCall />, name: 'Call Me', info: '(+20) 1273451052', to:'//wa.me/01273451052'},
         {id: 2,icon: <LuMail />, name: 'Email Me', info: 'youssef.turkey11@gmail.com', to:'mailto:youssef.turkey11@gmail.com'},
-        {id: 3,icon: <LuMapPin />, name: 'Visit Office', info: 'Alexandria, Egypt'}
+        {id: 3,icon: <LuMapPin />, name: 'Visit Office', info: 'Alexandria, Egypt', style: 'expand'}
       ]
     }
 }
@@ -37,11 +37,11 @@ const Contact = () => {
         <section className={`${styles.contact} mx-auto container my-40`} id='contact'>
             <Title data={data} />
 
-            <div className={`${styles.cards} grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-[10px] sm:gap-[20px] md:gap-[30px] my-10 md:mx-[85px] justify-items-center`}>
+            <div className={`${styles.cards} grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-[10px] sm:gap-[20px] md:gap-[30px]  my-10 md:mx-[85px] justify-items-center`}>
             {
                 cards.map( skill => {
                     return(
-                        <Link href={skill.to ? skill.to : ''} target='_blank' key={skill.id} className={`${styles.card} flex flex-col items-center gap-[15px] border border-bgHover hover:border-primary rounded-lg p-[40px] w-[250px] md:w-[100%] transition-all`}>
+                        <Link href={skill.to ? skill.to : ''} target='_blank' key={skill.id} className={`${styles.card} ${skill.style ? styles.expand : ''} flex flex-col items-center gap-[15px] border border-bgHover hover:border-primary rounded-lg p-[40px] w-[80%] md:w-[100%] transition-all`}>
                             <span className='bg-primary p-5 text-[40px] rounded-lg text-white'>{skill.icon}</span>
                             <p className='font-[500] text-[25px] text-color'>{skill.name}</p>
                             <p className='font-[500] text-[18px] text-secondary'>{skill.info}</p>
