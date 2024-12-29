7de332bbac55cf5bafef65d687bcdd8fa65f89d9
YoussefTurkey <youssef.turkey11@gmail.com>
2024-12-29 22:43:56 +0200

    counter btn

--- src/app/components/sectionComponents/elements/clientsNo/page.jsx
+++ src/app/components/sectionComponents/elements/clientsNo/page.jsx
@@ -1,14 +1,25 @@
-// importing style
+'use client'
+// importing react components
 import Image from "next/image";
+import { useState } from "react";
 // importing style
 import styles from "./clientsNo.module.scss";
 // importing components
 import Btn from "@/app/components/mainComponents/buttons/Btn";
+// importing react-icons
+import { FaPlus } from "react-icons/fa";
 
 const ClientsNumbers = () => {
+
+  // making counter
+    const [count, setCount] = useState(0);
+    const increment = () => {
+      setCount(count + 1)
+    }
+
   return (
     <section className={`${styles.clientsNo} md:pl-[30px] pl-[30px]`}>
-        <h3 className='text-[26px] font-[700] text-primary'>110+</h3>
+        <h3 className='text-[26px] font-[700] text-primary'>{count > 0 ? count + `+`: 0}</h3>
         <p className='text-[18px]'>Happy Clients</p>
 
         <div className={`${styles.imgs} flex my-5`}>
@@ -16,7 +27,8 @@ const ClientsNumbers = () => {
             <Image src='/images/photos/client02.svg' fetchPriority='low' width='50' height="50" alt='client02' loading='lazy' className="ml-[-1.3%]" />
             <Image src='/images/photos/client03.svg' fetchPriority='low' width='50' height="50" alt='client03' loading='lazy' className="ml-[-1.3%]" />
             <Image src='/images/photos/client04.svg' fetchPriority='low' width='50' height="50" alt='client04' loading='lazy' className="ml-[-1.3%]" />
-            <Image src='/images/photos/client05.svg' fetchPriority='low' width='50' height="50" alt='client05' loading='lazy' className="ml-[-1.3%]" />
+            {/* <Image src='/images/photos/client05.svg' fetchPriority='low' width='50' height="50" alt='client05' loading='lazy' className="ml-[-1.3%]" /> */}
+            <button className="bg-bgHover w-[65px] h-[65px] rounded-full border border-bgColor ml-[-1.3%]" onClick={increment}><FaPlus className="text-txtColor mx-auto text-2xl" /></button>
         </div>
 
         <Btn txt={'Portfolio'} />
